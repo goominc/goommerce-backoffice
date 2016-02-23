8bd95c7ffff9d85a11f5a76b561f9b7d0ab422eb
heekyu <heekyu83@gmail.com>
2016-02-23 16:36:20 +0900

    modify user roles

--- backoffice/user/controllers.js
+++ backoffice/user/controllers.js
@@ -1,7 +1,7 @@
 
 const userModule = require('./module');
 
-userModule.controller('UserManageController', ($scope, $http, $state, $rootScope, $translate) => {
+userModule.controller('UserManageController', ($scope, $http, $q, $state, $rootScope, $translate) => {
   $scope.contentTitle = $translate.instant('user.manage.title');
   $scope.contentSubTitle = '';
   $scope.breadcrumb = [
@@ -39,9 +39,8 @@ userModule.controller('UserManageController', ($scope, $http, $state, $rootScope
   $scope.newUser = {};
   $scope.createUser = (user) => {
     $http.post('/api/v1/users', user).then((res) => {
-      console.log(res);
       $http.post(`/api/v1/users/${res.data.id}/roles`, $scope.newUserRole).then(() => {
-        // $scope.closeUserPopup();
+        $scope.closeUserPopup();
         $state.reload();
       }).catch((err) => {
         window.alert(err.data.message);
@@ -53,8 +52,26 @@ userModule.controller('UserManageController', ($scope, $http, $state, $rootScope
   };
   $scope.closeUserPopup = () => {
     // 2016. 02. 23. [heekyu] modal hide is not work on page reload
-    // $('#user_manage_create_user').modal('hide');
-    $('.modal').removeClass('in');
+    $('#user_manage_create_user').modal('hide');
+    $('#user_manage_create_user').removeClass('in');
+    $('.modal-backdrop').remove();
+  };
+  $scope.editRole = { admin: false, buyer: false, seller: false };
+  $scope.makeUserRolePopupData = (user) => {
+    const res = { admin: false, buyer: false, seller: false };
+    for (let i = 0; i < user.roles.length; i++) {
+      const role = user.roles[i];
+      if (role.type === 'admin') {
+        res.admin = true;
+      } else if (role.type === 'buyer') {
+        res.buyer = true;
+      }
+    };
+    $scope.editRole = res;
+  };
+  $scope.closeRolePopup = () => {
+    $('#user_change_role').modal('hide');
+    $('#user_change_role').removeClass('in');
     $('.modal-backdrop').remove();
   };
   $scope.newUserPopup = {};
@@ -62,7 +79,116 @@ userModule.controller('UserManageController', ($scope, $http, $state, $rootScope
     $scope.newUserPopup.name = $translate.instant(`user.createUser.${role}`);
     $scope.newUserRole = { roleType: role };
   };
-  $scope.closeUserPopup();
+
+  const userIdToData = {};
+
+  $scope.datatablesLoaded = () => {
+    const datas = $('#user_list').find('table').DataTable().rows().data();
+    const children = $('#user_list').find('tbody').children();
+    for (let i = 0; i < datas.length; i++) {
+      const data = datas[i];
+      userIdToData[data.id] = data;
+
+      const child = children[i];
+      $(child).css('cursor', 'pointer');
+      $(child).click((e) => {
+        const userId = $(e.target).closest('tr').attr('id');
+        const user = userIdToData[userId];
+        $scope.editRoleUser = user;
+        $scope.makeUserRolePopupData(user);
+        if (!$scope.$$phase) {
+          $scope.$apply();
+        }
+        $('#user_change_role').modal();
+      });
+    }
+  };
+
+  // 2016. 02. 23. [heekyu] this is very limited since server cannot handle race condition properly
+  $scope.saveRole = () => {
+    const editRoleToData = () => {
+      if ($scope.editRole.admin) {
+        return [{ type: 'admin' }];
+      } else if ($scope.editRole.buyer) {
+        return [{ type: 'buyer' }];
+      }
+      return null;
+    };
+    const newRoleData = editRoleToData();
+    if (!newRoleData && !$scope.editRoleUser.roles) {
+      return;
+    }
+    const addOrDelete = {};
+
+    const isChangable = (roleType) => {
+      if (roleType === 'admin' || roleType === 'buyer') {
+        return true;
+      }
+      return false;
+    };
+
+    ($scope.editRoleUser.roles || []).forEach((role) => {
+      if (!isChangable(role.type)) {
+        return;
+      }
+      if (addOrDelete[role.type]) {
+        addOrDelete[role.type]--;
+      } else {
+        addOrDelete[role.type] = -1;
+      }
+    });
+    (newRoleData || []).forEach((role) => {
+      if (!isChangable(role.type)) {
+        return;
+      }
+      if (addOrDelete[role.type]) {
+        addOrDelete[role.type]++;
+      } else {
+        addOrDelete[role.type] = 1;
+      }
+    });
+    const url = `/api/v1/users/${$scope.editRoleUser.id}/roles`;
+    const keys = Object.keys(addOrDelete);
+    const addRoles = [];
+    const deleteRoles = [];
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const count = addOrDelete[key];
+      if (count === 1) {
+        addRoles.push(key);
+        // promises.push($http.post(url, { roleType: key }));
+      } else if (count === -1) {
+        deleteRoles.push(key);
+        // promises.push($http.delete(url, { data: { type: key }, headers: {"Content-Type": "application/json;charset=utf-8"} }));
+      }
+    }
+    if (deleteRoles.length > 0) {
+      $http.delete(url, { data: { type: deleteRoles[0] }, headers: {"Content-Type": "application/json;charset=utf-8"} }).then(() => {
+        if (addRoles.length > 0) {
+          $http.post(url, { roleType: addRoles[0] }).then(() => {
+            $scope.closeRolePopup();
+            $state.reload();
+          }).catch((err) => {
+            window.alert(err.message);
+          });
+        } else {
+          $scope.closeRolePopup();
+          $state.reload();
+        }
+      }).catch((err) => {
+        window.alert(err.message);
+      });
+    } else if (addRoles.length > 0) {
+      $http.post(url, { roleType: addRoles[0] }).then(() => {
+        $scope.closeRolePopup();
+        $state.reload();
+      }).catch((err) => {
+        window.alert(err.message);
+      });
+    } else {
+      $scope.closeRolePopup();
+    }
+  };
 });
 
 userModule.controller('UserWaitConfirmController', ($scope, $state, $rootScope, $translate) => {
--- dist/duo/main/index.js
+++ dist/duo/main/index.js
@@ -641,6 +641,9 @@ module.exports = {
     },
     "waitConfirm": {
       "title": "바이어 인증 대기"
+    },
+    "role": {
+      "changeTitle": "유저 권한 변경"
     }
   }
 }
@@ -651,7 +654,7 @@ module.exports = {
 
 var userModule = require('./module');
 
-userModule.controller('UserManageController', function ($scope, $http, $state, $rootScope, $translate) {
+userModule.controller('UserManageController', function ($scope, $http, $q, $state, $rootScope, $translate) {
   $scope.contentTitle = $translate.instant('user.manage.title');
   $scope.contentSubTitle = '';
   $scope.breadcrumb = [{
@@ -682,9 +685,8 @@ userModule.controller('UserManageController', function ($scope, $http, $state, $
   $scope.newUser = {};
   $scope.createUser = function (user) {
     $http.post('/api/v1/users', user).then(function (res) {
-      console.log(res);
       $http.post('/api/v1/users/' + res.data.id + '/roles', $scope.newUserRole).then(function () {
-        // $scope.closeUserPopup();
+        $scope.closeUserPopup();
         $state.reload();
       })['catch'](function (err) {
         window.alert(err.data.message);
@@ -696,8 +698,26 @@ userModule.controller('UserManageController', function ($scope, $http, $state, $
   };
   $scope.closeUserPopup = function () {
     // 2016. 02. 23. [heekyu] modal hide is not work on page reload
-    // $('#user_manage_create_user').modal('hide');
-    $('.modal').removeClass('in');
+    $('#user_manage_create_user').modal('hide');
+    $('#user_manage_create_user').removeClass('in');
+    $('.modal-backdrop').remove();
+  };
+  $scope.editRole = { admin: false, buyer: false, seller: false };
+  $scope.makeUserRolePopupData = function (user) {
+    var res = { admin: false, buyer: false, seller: false };
+    for (var i = 0; i < user.roles.length; i++) {
+      var role = user.roles[i];
+      if (role.type === 'admin') {
+        res.admin = true;
+      } else if (role.type === 'buyer') {
+        res.buyer = true;
+      }
+    };
+    $scope.editRole = res;
+  };
+  $scope.closeRolePopup = function () {
+    $('#user_change_role').modal('hide');
+    $('#user_change_role').removeClass('in');
     $('.modal-backdrop').remove();
   };
   $scope.newUserPopup = {};
@@ -705,7 +725,116 @@ userModule.controller('UserManageController', function ($scope, $http, $state, $
     $scope.newUserPopup.name = $translate.instant('user.createUser.' + role);
     $scope.newUserRole = { roleType: role };
   };
-  $scope.closeUserPopup();
+
+  var userIdToData = {};
+
+  $scope.datatablesLoaded = function () {
+    var datas = $('#user_list').find('table').DataTable().rows().data();
+    var children = $('#user_list').find('tbody').children();
+    for (var i = 0; i < datas.length; i++) {
+      var data = datas[i];
+      userIdToData[data.id] = data;
+
+      var child = children[i];
+      $(child).css('cursor', 'pointer');
+      $(child).click(function (e) {
+        var userId = $(e.target).closest('tr').attr('id');
+        var user = userIdToData[userId];
+        $scope.editRoleUser = user;
+        $scope.makeUserRolePopupData(user);
+        if (!$scope.$$phase) {
+          $scope.$apply();
+        }
+        $('#user_change_role').modal();
+      });
+    }
+  };
+
+  // 2016. 02. 23. [heekyu] this is very limited since server cannot handle race condition properly
+  $scope.saveRole = function () {
+    var editRoleToData = function editRoleToData() {
+      if ($scope.editRole.admin) {
+        return [{ type: 'admin' }];
+      } else if ($scope.editRole.buyer) {
+        return [{ type: 'buyer' }];
+      }
+      return null;
+    };
+    var newRoleData = editRoleToData();
+    if (!newRoleData && !$scope.editRoleUser.roles) {
+      return;
+    }
+    var addOrDelete = {};
+
+    var isChangable = function isChangable(roleType) {
+      if (roleType === 'admin' || roleType === 'buyer') {
+        return true;
+      }
+      return false;
+    };
+
+    ($scope.editRoleUser.roles || []).forEach(function (role) {
+      if (!isChangable(role.type)) {
+        return;
+      }
+      if (addOrDelete[role.type]) {
+        addOrDelete[role.type]--;
+      } else {
+        addOrDelete[role.type] = -1;
+      }
+    });
+    (newRoleData || []).forEach(function (role) {
+      if (!isChangable(role.type)) {
+        return;
+      }
+      if (addOrDelete[role.type]) {
+        addOrDelete[role.type]++;
+      } else {
+        addOrDelete[role.type] = 1;
+      }
+    });
+    var url = '/api/v1/users/' + $scope.editRoleUser.id + '/roles';
+    var keys = Object.keys(addOrDelete);
+    var addRoles = [];
+    var deleteRoles = [];
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      var count = addOrDelete[key];
+      if (count === 1) {
+        addRoles.push(key);
+        // promises.push($http.post(url, { roleType: key }));
+      } else if (count === -1) {
+          deleteRoles.push(key);
+          // promises.push($http.delete(url, { data: { type: key }, headers: {"Content-Type": "application/json;charset=utf-8"} }));
+        }
+    }
+    if (deleteRoles.length > 0) {
+      $http['delete'](url, { data: { type: deleteRoles[0] }, headers: { "Content-Type": "application/json;charset=utf-8" } }).then(function () {
+        if (addRoles.length > 0) {
+          $http.post(url, { roleType: addRoles[0] }).then(function () {
+            $scope.closeRolePopup();
+            $state.reload();
+          })['catch'](function (err) {
+            window.alert(err.message);
+          });
+        } else {
+          $scope.closeRolePopup();
+          $state.reload();
+        }
+      })['catch'](function (err) {
+        window.alert(err.message);
+      });
+    } else if (addRoles.length > 0) {
+      $http.post(url, { roleType: addRoles[0] }).then(function () {
+        $scope.closeRolePopup();
+        $state.reload();
+      })['catch'](function (err) {
+        window.alert(err.message);
+      });
+    } else {
+      $scope.closeRolePopup();
+    }
+  };
 });
 
 userModule.controller('UserWaitConfirmController', function ($scope, $state, $rootScope, $translate) {
