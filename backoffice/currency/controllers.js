81db60429df61a1a98d962778d32bb5e7a2edb8f
heekyu <heekyu83@gmail.com>
2016-02-23 11:23:47 +0900

    attach currency API

--- backoffice/currency/controllers.js
+++ backoffice/currency/controllers.js
@@ -3,6 +3,22 @@
 const currencyModule = require('./module');
 
 currencyModule.controller('CurrencyMainController', ($scope, $http) => {
+  $scope.rates = {
+    USD: 0,
+    CNY: 0,
+  };
+  $http.get('/api/v1/currency').then((res) => {
+    if (res.data.USD) {
+      $scope.rates.USD = res.data.USD;
+    }
+    if (res.data.CNY) {
+      $scope.rates.CNY = res.data.CNY;
+    }
+  });
   $scope.save = () => {
+    $http.post('/api/v1/currency', $scope.rates).then(() => {}).catch((res) => {
+      window.alert(res);
+      console.log(res);
+    });
   };
 });
