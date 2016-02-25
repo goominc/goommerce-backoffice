c0eb83a756cbf7362752a46bb0152ea9cb5319df
khu <hukim99@gmail.com>
2016-02-25 13:52:24 +0900

    Server message.

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 // Only for test backoffice behaviours. Please do not use this in production.
 const express = require('express');
 const app = express();
+const port = 8282;
 
 app.use('/bower_components', express.static('bower_components'));
 app.use(express.static('dist'));
 
-app.listen(8282, () => {});
+app.listen(port, () => {});
+console.log('Server is listening on', port);
