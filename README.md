8d9d2b3c8c1c7edf7b5ba314e80635bdb9378eba
khu <hukim99@gmail.com>
2016-02-25 13:26:34 +0900

    Update README for testing.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-goommerce backoffice
+Goommerce Back Office
+=====================
 
-current UI platform is 'metronic'
+We use '[Metronic](http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469)' as a template. You should buy it before use.
 
+For testing,
+
+> $ cd backoffice
+>
+> $ gulp --dev
+>
+> $ cd ..
+>
+> $ bower install
+>
+> $ npm install
+>
+> $ node server
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
+// Copyright (C) 2016 Goom Inc. All rights reserved.
 
-// Only for test backoffice behaviours
-// Do not use in production
+// Only for test backoffice behaviours. Please do not use this in production.
 const express = require('express');
 const app = express();
 
 app.use('/bower_components', express.static('bower_components'));
 app.use(express.static('dist'));
 
-
-app.listen(8282, () => {
-});
+app.listen(8282, () => {});
