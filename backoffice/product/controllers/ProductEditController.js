7f19eeddbb6fe35ba4b0f04002db7fca04757123
durante <durante0907@gmail.com>
2017-01-19 18:07:38 +0900

    add color code.

--- backoffice/product/controllers/ProductEditController.js
+++ backoffice/product/controllers/ProductEditController.js
@@ -6,7 +6,7 @@ productModule.controller('ProductEditController', ($scope, $http, $state, $rootS
   $scope.allColors = {
     BLUE: ['RBL', 'BLU', 'VBL', 'NVY', 'MNT', 'CBL', 'DNV', 'CBT', 'LBL', 'DBR', 'DBL', 'TBL', 'PUR'],
     GREEN: ['GRN', 'AGN', 'KHA', 'BGN', 'DGN'],
-    RED: ['RED', 'PNK', 'WNE', 'LOG', 'ORG', 'POR', 'BGD', 'DRD', 'LRD'],
+    RED: ['RED', 'PNK', 'WNE', 'LOG', 'ORG', 'POR', 'BGD', 'DRD', 'LRD', 'BNR'],
     GRAY: ['GRY', 'LGR', 'CGR', 'MGR', 'CMG', 'DGR'],
     WHITE: ['WHT', 'OWH', 'IVY', 'BEG', 'LBI'],
     OTHERS: ['BLK', 'SLV', 'CMF', 'PBK', 'PWH', 'PTN', 'SLM', 'NYE', 'YEL', 'AWT', 'ABL', 'BWR', 'WNB', 'BRW', 'BNW', 'BNG'],
