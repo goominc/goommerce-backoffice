cab7e926775b7200375313ac18e684647f64970e
changhyup Cho <andy@changhyups-iMac.local>
2017-05-04 17:02:15 +0900

    Add color code.

--- backoffice/product/controllers/ProductEditController.js
+++ backoffice/product/controllers/ProductEditController.js
@@ -6,7 +6,7 @@ productModule.controller('ProductEditController', ($scope, $http, $state, $rootS
   $scope.allColors = {
     BLUE: ['RBL', 'BLU', 'VBL', 'NVY', 'MNT', 'CBL', 'DNV', 'CBT', 'LBL', 'DBR', 'DBL', 'TBL', 'PUR', 'MBL'],
     GREEN: ['GRN', 'AGN', 'KHA', 'BGN', 'DGN'],
-    RED: ['RED', 'PNK', 'WNE', 'LOG', 'ORG', 'POR', 'BGD', 'DRD', 'LRD', 'BNR'],
+    RED: ['RED', 'PNK', 'WNE', 'LOG', 'ORG', 'POR', 'BGD', 'DRD', 'LRD', 'BNR', 'HPK', 'COR'],
     GRAY: ['GRY', 'LGR', 'CGR', 'MGR', 'CMG', 'DGR'],
     WHITE: ['WHT', 'OWH', 'IVY', 'BEG', 'LBI', 'LBE'],
     OTHERS: ['BLK', 'SLV', 'CMF', 'PBK', 'PWH', 'PTN', 'SLM', 'NYE', 'YEL', 'AWT', 'ABL', 'BWR', 'WNB', 'BRW', 'BNW', 'BNG', 'BNS'],
