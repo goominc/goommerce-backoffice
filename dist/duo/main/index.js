e44b32dba745b7aba8d25167cc6533c0436f7eeb
durante <durante0907@gmail.com>
2017-05-19 16:09:06 +0900

    deploy

--- dist/duo/main/index.js
+++ dist/duo/main/index.js
@@ -5824,7 +5824,7 @@ var productModule = require('../module.js');
 
 productModule.controller('ProductEditController', function ($scope, $http, $state, $rootScope, $translate, product, categories, productUtil, boUtils) {
   $scope.allColors = {
-    BLUE: ['RBL', 'BLU', 'VBL', 'NVY', 'MNT', 'CBL', 'DNV', 'CBT', 'LBL', 'DBR', 'DBL', 'TBL', 'PUR', 'MBL'],
+    BLUE: ['RBL', 'BLU', 'VBL', 'NVY', 'MNT', 'CBL', 'DNV', 'CBT', 'LBL', 'DBR', 'DBL', 'TBL', 'PUR', 'MBL', 'PBL'],
     GREEN: ['GRN', 'AGN', 'KHA', 'BGN', 'DGN', 'LIM', 'LMT'],
     RED: ['RED', 'PNK', 'WNE', 'LOG', 'ORG', 'POR', 'BGD', 'DRD', 'LRD', 'BNR', 'HPK', 'COR'],
     GRAY: ['GRY', 'LGR', 'CGR', 'MGR', 'CMG', 'DGR'],
